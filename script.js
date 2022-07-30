1cfba0b1d803255639adccf90a0df915c87bf9d1
nachhth <nachito.th7@gmail.com>
2022-07-30 16:37:29 +0200

    finished

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 'use strict';
 
 const container = document.querySelector('#container');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    removeActiveClasses();
+    card.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  cards.forEach((card) => {
+    card.classList.remove('active');
+  });
+}
